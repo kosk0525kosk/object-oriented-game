75e7c96f0dbc243426aa61588b1954ce6e1dec4e
ksy0525 <kosuke.fukasawa@gmail.com>
2021-04-27 21:38:54 +0900

    微調整

--- sketch.js
+++ sketch.js
@@ -13,8 +13,8 @@ function draw(){
 
     // 座標系の変換
     rectMode(CENTER);
-    translate(width/2, height/2);
-    scale(1, -1);
+    translate(width/2, height/2); // 原点をキャンバスの中心にする
+    scale(1, -1); // y軸の正の方向が上方向となるようにする
 
     // プレイヤー移動
     player.move();
